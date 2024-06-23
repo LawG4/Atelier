128ed3c1b2d9c78f115482abe792950351ff2c81
Lawrence <61335367+LawG4@users.noreply.github.com>
2024-06-23 13:57:13 +0100

    Add stuppid logger

--- include/atelier/atelier.h
+++ include/atelier/atelier.h
@@ -1,10 +1,23 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
-#include <windef.h>
+#include <windows.h>
 
 namespace Atelier
 {
+
+/**
+ * @brief Stupid cursed logger. Uses cursed static elements, so I'm not sure the best way to share between dynamic
+ * loadable elements
+ */
+struct Log {
+    static void init();
+    static void shutdown();
+    static void unformatted(const char* const msg);
+    static void info(const char* const msg, ...);
+    static void warn(const char* const msg, ...);
+    static void error(const char* const msg, ...);
+};
 struct MainWindow {
     MainWindow() = default;
     bool should_continue = true;
---
+++ source/logger.cpp
@@ -0,0 +1,65 @@
+#include <varargs.h>
+#include "atelier/atelier.h"
+
+static std::string fmt_va(const char* const msg, va_list va_args)
+{
+    // How many chars we need
+    size_t required_size = vsnprintf(nullptr, 0, msg, va_args);
+
+    // vsn printf doesn't count null terminator!
+    std::string out;
+    out.resize(required_size + 1, '\0');
+
+    // Perform the format in the buffer
+    vsnprintf(out.data(), out.size(), msg, va_args);
+    return out;
+}
+
+void Atelier::Log::init()
+{
+#ifndef NDEBUG
+    // Do we have a console attached in win32?
+    if (!AttachConsole(ATTACH_PARENT_PROCESS)) {
+        AllocConsole();
+        freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+    }
+
+#endif
+}
+
+void Atelier::Log::shutdown() {}
+
+void Atelier::Log::unformatted(const char* const msg) { printf("%s", msg); }
+
+void Atelier::Log::info(const char* const msg, ...)
+{
+    va_list args;
+    va_start(args, msg);
+    auto fmt = fmt_va(msg, args);
+    va_end(args);
+
+    fmt = "INFO: " + fmt + "\n";
+    unformatted(fmt.c_str());
+}
+
+void Atelier::Log::warn(const char* const msg, ...)
+{
+    va_list args;
+    va_start(args, msg);
+    auto fmt = fmt_va(msg, args);
+    va_end(args);
+
+    fmt = "WARN: " + fmt + "\n";
+    unformatted(fmt.c_str());
+}
+
+void Atelier::Log::error(const char* const msg, ...)
+{
+    va_list args;
+    va_start(args, msg);
+    auto fmt = fmt_va(msg, args);
+    va_end(args);
+
+    fmt = "ERROR: " + fmt + "\n";
+    unformatted(fmt.c_str());
+}
